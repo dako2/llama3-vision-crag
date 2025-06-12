1902279f97fdeb4e46d90ef8eb1bc051eb0fd07b
Yi <yihan7206@gmail.com>
2025-06-12 13:59:26 -0700

    tune batch size

--- sft_data_gen.py
+++ sft_data_gen.py
@@ -29,7 +29,7 @@ from itertools import islice
 ############################
 # Config
 ############################
-BATCH_SIZE = 16                    # ← change to fit GPU/VRAM
+BATCH_SIZE = 8                    # ← change to fit GPU/VRAM
 SPLIT      = "validation"         # or "public_test", etc.
 
 
