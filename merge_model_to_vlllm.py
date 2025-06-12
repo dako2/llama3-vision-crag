8346ffb050cf214584ce5d1425199b3d895350d6
Yi <yihan7206@gmail.com>
2025-06-12 13:09:32 -0700

    merge

--- merge_model_to_vlllm.py
+++ merge_model_to_vlllm.py
@@ -1,10 +1,23 @@
+# from unsloth import FastVisionModel
+# from transformers import AutoTokenizer
+
+# # Load base model and fine-tuned adapter
+# model_id = "unsloth/Llama-3.2-11B-Vision-Instruct"
+# model, tokenizer = FastVisionModel.from_pretrained(model_id, load_in_4bit=False)
+# model.load_adapter("llama3-vision-finetuned_full")
+
+# # Merge and save in 16-bit HuggingFace format (for vLLM or transformers)
+# model.save_pretrained_merged("llama3-vision-merged-vllm", tokenizer, save_method="merged_16bit")
+
+
 from unsloth import FastVisionModel
-from transformers import AutoTokenizer
+model, tokenizer = FastVisionModel.from_pretrained(
+    model_name = "trainer_output/checkpoint-12",
+    max_seq_length = 8192,
+    #dtype = dtype,
+    load_in_4bit = True, # still 4-bits as trained model
+)
 
-# Load base model and fine-tuned adapter
-model_id = "unsloth/Llama-3.2-11B-Vision-Instruct"
-model, tokenizer = FastVisionModel.from_pretrained(model_id, load_in_4bit=False)
-model.load_adapter("llama3-vision-finetuned_full")
-
-# Merge and save in 16-bit HuggingFace format (for vLLM or transformers)
-model.save_pretrained_merged("llama3-vision-merged-vllm", tokenizer, save_method="merged_16bit")
+# Saving & Pushing
+model.save_pretrained_merged("model", tokenizer, save_method = "merged_16bit",)
+#model.push_to_hub_merged("hf/model", tokenizer, save_method = "merged_16bit", token = "")
