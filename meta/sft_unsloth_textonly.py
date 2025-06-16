28628d898895459208cdb698d6b2a916e4136e05
oyiyi <qitang2023@gmail.com>
2025-06-15 22:49:02 -0700

    a

--- meta/sft_unsloth_textonly.py
+++ meta/sft_unsloth_textonly.py
@@ -1,5 +1,7 @@
 from unsloth import FastLanguageModel
 import torch
+from datasets import load_dataset
+
 max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
 dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
 load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
