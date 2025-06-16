0a614a4a8af7112a7ee12ef25453a2674dee4f2e
oyiyi <qitang2023@gmail.com>
2025-06-16 12:04:12 -0700

    zeroshot

--- meta/agents/user_config.py
+++ meta/agents/user_config.py
@@ -1,5 +1,5 @@
-from agents.rag_agent import SimpleRAGAgent
-#from agents.random_agent import RandomAgent
+#from agents.rag_agent import SimpleRAGAgent
+from agents.my_agent_zero_shot import SimpleRAGAgent
 #from agents.vanilla_llama_vision_agent import LlamaVisionModel
 
 #UserAgent = RandomAgent
