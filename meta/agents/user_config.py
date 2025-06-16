13a94fc072c0a5c2fbe8ab38cd2c6ba65eb534ec
oyiyi <qitang2023@gmail.com>
2025-06-16 13:44:06 -0700

    tuning

--- meta/agents/user_config.py
+++ meta/agents/user_config.py
@@ -1,5 +1,5 @@
-#from agents.rag_agent import SimpleRAGAgent
-from agents.my_agent_zero_shot import SimpleRAGAgent
+from agents.rag_agent import SimpleRAGAgent
+#from agents.my_agent_zero_shot import SimpleRAGAgent
 #from agents.vanilla_llama_vision_agent import LlamaVisionModel
 
 #UserAgent = RandomAgent
